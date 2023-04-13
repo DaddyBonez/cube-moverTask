9c2f83abcebfd4816abd28955e73c632d82a2954
DaddyBonez <joref04@gmail.com>
2023-04-13 09:43:37 +0200

    fghk

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
-var box = document.getElementById('box');
-var screenwidth = window.innerWidth;
-var screenheight = window.innerHeight;
+let box = document.querySelector('.box');
+let moveby = 10;
 
-document.addEventListener('keydown', function(event) {
-    if (event.KeyboardEvent === 37) { 
-        var newLeft = parseInt(box.style.left) - 10;
-        if (newLeft >= 0) {
-          box.style.left = newLeft + 'px';
-        }
-    } else if (event.key === 38) { 
-        var newTop = parseInt(box.style.top) - 10;
-        if (newTop >= 0) {
-          box.style.top = newTop + 'px';
-        }
-    } else if (event.key === 39) { 
-        var newLeft = parseInt(box.style.left) + 10;
-        if (newLeft + box.offsetWidth <= screenwidth) {
-          box.style.left = newLeft + 'px';
-        }
-    } else if (event.key === 40) { 
-        var newTop = parseInt(box.style.top) + 10;
-        if (newTop + box.offsetHeight <= screenheight) {
-          box.style.top = newTop + 'px';
-        }
+window.addEventListener('load', () =>{
+    box.style.left = "200px";
+    box.style.top = 0;
+});
+
+window.addEventListener('keydown', (e) =>{
+
+    console.log(load)
+    switch(e.key){
+        case 'ArrowLeft' :
+            box.style.left = parseInt(box.style.left) - moveBy + 'px'
+            break;
+        case 'ArrowRight' :    
+            box.style.left = parseInt(box.style.left) + moveBy + 'px'
+            break;
+        case 'ArrowUp' :    
+            box.style.top = parseInt(box.style.top) - moveBy + 'px'
+            break;
+        case 'ArrowDown' :   
+            box.style.top = parseInt(box.style.top) + moveBy + 'px'
+            break;
     }
-
-    console.log(event.key);
 });
