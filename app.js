1091881bba362a28bfb547effc87d2e429af058d
DaddyBonez <joref04@gmail.com>
2023-04-14 08:35:44 +0200

    some changes

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 let box = document.querySelector('.box');
 let moveby = "10px";
 
-window.addEventListener('load', () =>{
+window.addEventListener('load', () => {
     box.style.left = "200px";
     box.style.top = "200px";
 });
 
 window.addEventListener('keydown', (e) =>{
 
-    console.log()
+    console.log(box.style.left = parseInt(box.style.left) - moveBy + 'px')
     switch(e.key){
         case 'ArrowLeft' :
             box.style.left = parseInt(box.style.left) - moveBy + 'px'
